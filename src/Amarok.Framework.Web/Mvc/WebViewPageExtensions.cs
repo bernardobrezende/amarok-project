2ea10c8ee74a91bcdaff0397599cbb2de390be00
Bernardo B Rezende <bernardobrezende@gmail.com>
2011-08-02 21:47:31 -0300

    Adding Amarok's Web class libraries with some MVC Extension Methods for localization and globalization.

---
+++ src/Amarok.Framework.Web/Mvc/WebViewPageExtensions.cs
@@ -0,0 +1,25 @@
+﻿using System.Web;
+using System.Web.Mvc;
+
+namespace Amarok.Framework.Web.Mvc
+{
+    public static class WebViewPageExtensions
+    {
+        public static T GetLocalResource<T>(this WebViewPage page, string resourceKey)
+        {
+            var context = GetPageHttpContext(page);
+            return (T)context.GetLocalResourceObject(page.VirtualPath, resourceKey);
+        }
+
+        public static T GetGlobalResource<T>(this WebViewPage page, string resourceName, string resourceKey)
+        {
+            var context = GetPageHttpContext(page);
+            return (T)context.GetGlobalResourceObject(resourceName, resourceKey);
+        }
+
+        private static HttpContextBase GetPageHttpContext(WebViewPage page)
+        {
+            return page.ViewContext.HttpContext;
+        }
+    }
+}

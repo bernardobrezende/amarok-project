5c3122c36a8b9de769a8ccfb4cc78894a97ec101
Bernardo B Rezende <bernardobrezende@gmail.com>
2011-08-02 21:52:53 -0300

    Adding UrlHelper's Extension Method for writing Friendly-Url

---
+++ src/Amarok.Framework.Web/Mvc/UrlHelperExtensions.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Text;
+using System.Web.Mvc;
+
+namespace Amarok.Framework.Web.Mvc
+{
+    public static class UrlHelperExtensions
+    {
+        public static string ToFriendlyUrl(this UrlHelper helper, string urlToEncode)
+        {
+            urlToEncode = (urlToEncode ?? String.Empty).Trim().ToLower();
+
+            StringBuilder url = new StringBuilder();
+
+            foreach (char ch in urlToEncode)
+            {
+                switch (ch)
+                {
+                    case ' ':
+                        url.Append('-');
+                        break;
+                    case '&':
+                        url.Append("and");
+                        break;
+                    case '\'':
+                        break;
+                    default:
+                        if ((ch >= '0' && ch <= '9') || (ch >= 'a' && ch <= 'z'))
+                        {
+                            url.Append(ch);
+                        }
+                        else
+                        {
+                            url.Append('-');
+                        }
+                        break;
+                }
+            }
+            return url.ToString();
+        }
+    }
+}

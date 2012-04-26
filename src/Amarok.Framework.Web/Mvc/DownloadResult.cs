6d2399aecbc34bb187ed95d5f86a17116c223f14
Bernardo Bosak de Rezende <bernardobrezende@gmail.com>
2012-04-26 14:32:46 -0300

    ADD: DownloadResult class for ASP.NET MVC actions.

---
+++ src/Amarok.Framework.Web/Mvc/DownloadResult.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Web.Mvc;
+
+namespace Amarok.Framework.Web.Mvc
+{
+    public class DownloadResult : ActionResult
+    {
+        public DownloadResult()
+        {
+        }
+
+        public DownloadResult(string virtualPath)
+        {
+            this.VirtualPath = virtualPath;
+        }
+
+        public string VirtualPath { get; set; }
+        public string FileDownloadName { get; set; }
+
+        public override void ExecuteResult(ControllerContext context)
+        {
+            if (!String.IsNullOrEmpty(FileDownloadName))
+            {
+                context.HttpContext.Response.AddHeader("content-disposition", "attachment; filename=" + this.FileDownloadName);
+            }
+
+            string filePath = context.HttpContext.Server.MapPath(this.VirtualPath);
+            context.HttpContext.Response.TransmitFile(filePath);
+        }
+    }
+}

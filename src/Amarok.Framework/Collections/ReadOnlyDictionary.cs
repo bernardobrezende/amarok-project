62be199725403fed11f0c1c1dc46036451bc868c
Bernardo Bosak de Rezende <bernardobrezende@gmail.com>
2012-04-27 12:23:49 -0300

    ADD: Read-only dictionary collection class.

---
+++ src/Amarok.Framework/Collections/ReadOnlyDictionary.cs
@@ -0,0 +1,124 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Amarok.Framework.Collections
+{
+    public class ReadOnlyDictionary<TKey, TValue> : IDictionary<TKey, TValue>
+    {
+        private const string READONLY_MESSAGE = "This is a read-only collection.";
+        private IDictionary<TKey, TValue> _dictionary;
+
+        public ReadOnlyDictionary()
+        {
+            _dictionary = new Dictionary<TKey, TValue>();
+        }
+
+        public ReadOnlyDictionary(IDictionary<TKey, TValue> dictionary)
+        {
+            _dictionary = dictionary;
+        }
+
+        #region IDictionary<TKey,TValue> Members
+
+        public void Add(TKey key, TValue value)
+        {
+            throw new NotSupportedException(READONLY_MESSAGE);
+        }
+
+        public bool ContainsKey(TKey key)
+        {
+            return _dictionary.ContainsKey(key);
+        }
+
+        public ICollection<TKey> Keys
+        {
+            get { return _dictionary.Keys; }
+        }
+
+        public bool Remove(TKey key)
+        {
+            throw new NotSupportedException(READONLY_MESSAGE);
+        }
+
+        public bool TryGetValue(TKey key, out TValue value)
+        {
+            return _dictionary.TryGetValue(key, out value);
+        }
+
+        public ICollection<TValue> Values
+        {
+            get { return _dictionary.Values; }
+        }
+
+        public TValue this[TKey key]
+        {
+            get
+            {
+                return _dictionary[key];
+            }
+            set
+            {
+                throw new NotSupportedException(READONLY_MESSAGE);
+            }
+        }
+
+        #endregion
+
+        #region ICollection<KeyValuePair<TKey,TValue>> Members
+
+        public void Add(KeyValuePair<TKey, TValue> item)
+        {
+            throw new NotSupportedException(READONLY_MESSAGE);
+        }
+
+        public void Clear()
+        {
+            throw new NotSupportedException(READONLY_MESSAGE);
+        }
+
+        public bool Contains(KeyValuePair<TKey, TValue> item)
+        {
+            return _dictionary.Contains(item);
+        }
+
+        public void CopyTo(KeyValuePair<TKey, TValue>[] array, int arrayIndex)
+        {
+            _dictionary.CopyTo(array, arrayIndex);
+        }
+
+        public int Count
+        {
+            get { return _dictionary.Count; }
+        }
+
+        public bool IsReadOnly
+        {
+            get { return true; }
+        }
+
+        public bool Remove(KeyValuePair<TKey, TValue> item)
+        {
+            throw new NotSupportedException(READONLY_MESSAGE);
+        }
+
+        #endregion
+
+        #region IEnumerable<KeyValuePair<TKey,TValue>> Members
+
+        public IEnumerator<KeyValuePair<TKey, TValue>> GetEnumerator()
+        {
+            return _dictionary.GetEnumerator();
+        }
+
+        #endregion
+
+        #region IEnumerable Members
+
+        System.Collections.IEnumerator System.Collections.IEnumerable.GetEnumerator()
+        {
+            return (_dictionary as System.Collections.IEnumerable).GetEnumerator();
+        }
+
+        #endregion
+    }
+}

8f1345a032389de46bce2d69052457c217c31cf4
Bernardo Bosak de Rezende <bernardobrezende@gmail.com>
2012-04-30 09:40:22 -0300

    ADD: Adding extension method for IEnumerable types that enumerates only the not nullable elements of the collection.

---
+++ src/Amarok.Framework/Extensions/IEnumerableExtensions.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Amarok.Framework.Extensions
+{
+    public static class IEnumerableExtensions
+    {
+        public static IEnumerable<T> OnlyNotNull<T>(this IEnumerable<T> collection)
+        {
+            return collection != null ? collection.Where(x => x != null) : Activator.CreateInstance<List<T>>();
+        }
+    }
+}
